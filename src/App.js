a27f8a7360693bb51efe0baea71d13f5363f7404
john681611 <john681611@hotmail.com>
2020-09-22 23:30:50 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import FilterableTable from 'react-filterable-table'
+import Accordion from 'react-bootstrap/Accordion'
+import Card from 'react-bootstrap/Card'
 import './App.css';
 
 function App() {
+  const [initialData, setInitialData] = React.useState([]);
+  const fields = [
+    { type: "text", required: true, name: 'nickname', displayName: "Nickname", inputFilterable: true, sortable: true },
+    { type: "text", required: true, name: 'fcname', displayName: "Carrier Name", inputFilterable: true, exactFilterable: true, sortable: true },
+    { type: "text", required: true, name: 'route', displayName: "Route", inputFilterable: true, exactFilterable: true, sortable: true },
+    { type: "date", required: true, name: 'depDate', displayName: "Departure Date", inputFilterable: true, exactFilterable: true, sortable: true },
+    { type: "date", required: true, name: 'destDate', displayName: "Arrival Date", inputFilterable: true, exactFilterable: true, sortable: true },
+    { type: "text", name: 'notes', displayName: "Notes", inputFilterable: true }
+  ];
+  useEffect(() => {
+    const callApi = async () => {
+      try {
+        const data = await fetch('https://ironfists.azurewebsites.net/trip').then(result => result.json())
+        setInitialData(data);
+      } catch (error) {
+        console.error("useEffect: ", error)
+      }
+    }
+    callApi();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Elite Trip Board</h1>
+      <FilterableTable
+        namespace="People"
+        initialSort="name"
+        data={initialData}
+        fields={fields}
+        noRecordsMessage="There are no trips to display"
+        noFilteredRecordsMessage="No trips match your filters!"
+      />
+      <Accordion defaultActiveKey="0">
+        <Card>
+          <Card.Header>
+            <Accordion.Toggle as={Card.Header} variant="link" eventKey="1">
+              Log Your trip
+        </Accordion.Toggle>
+          </Card.Header>
+          <Accordion.Collapse eventKey="1">
+            <Card.Body>
+              <p>Note: This is quickly hacked up and therefore its not easy to remove stuff be careful</p>
+              <form method="POST" action="https://ironfists.azurewebsites.net/trip">
+                <div class="form-group">
+                  {fields.map(element =>
+                    <span>
+                      <label for={element.name}>
+                        {element.displayName}
+                      </label>
+                      <input id={element.name} name={element.name} type={element.type} class="form-control" required={element.required} />
+                    </span>
+                  )}
+                </div>
+                <button type="submit" class="btn btn-warning">Submit</button>
+              </form>
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      </Accordion>
+
     </div>
   );
 }
